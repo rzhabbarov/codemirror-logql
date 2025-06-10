baff161c4fae7cf0a824123c5fb51534e904df41
Ramil Zhabbarov <were-wolff@yandex.ru>
2025-06-10 10:09:06 +0300

    removed unused file

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Extension } from '@codemirror/state';
-
-export interface LogQlExtensionConfig {
-    lokiUrl?: string,
-    enableCompletion?: boolean,
-    themeExtension?: Extension,
-    optionClass?: string;
-}
